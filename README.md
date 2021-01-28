9980f6928b421a9500e60b9c46c703711c90191f
Elliott Silverman <elliotts@ualberta.ca>
2021-01-28 16:38:44 -0700

    Adding Calculator Project

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 ***READ ME***
 
-In this repository I am going to start using javascript in combination with HTML and CSS to create a UI for the RPS game I created previously. I am going to try and do a few more advanced techniques to really push myself as I still neeed lots of practice to become confident.
+This project is going to create a fully functional calculator. The calculator will add, subtract, divide, and multiply. The display will live update and it will string together long equations of numbers. Optionally I am going to try and program a backspace button, floating point number support, and keyboard support as well. 
+
+I will be using CSS grid for the layout and basic Javascript with multiple event listeners/DOM manipulation for the functionality of the buttons.
--- script.js
+++ script.js
@@ -1,122 +1,361 @@
-const rockBtn = document.querySelector(".rock");
-const paperBtn = document.querySelector(".paper");
-const scissorsBtn = document.querySelector(".scissors");
-const playAgainBtn = document.querySelector(".playAgain");
-let resultMessage = document.querySelector(".results");
-let winCounter = document.querySelector(".playerScoreCount");
-let loseCounter = document.querySelector(".computerScoreCount");
-let drawCounter = document.querySelector(".drawScoreCount");
+//INTIAL VARIABLES
+const display = document.getElementById("display");
+const container = document.querySelector(".container");
+let prev = 0;
+let curr = 0;
+let chosenOperator = "";
 
-let playerInput = "";
-let playerWins = 0;
-let computerWins = 0;
-let draws = 0;
+//CALCULATOR FUNCTIONS
+const add = (prev, curr) => prev + curr;
+const subtract = (prev, curr) => prev - curr;
+const multiply = (prev, curr) => prev * curr;
+const divide = (prev, curr) => prev / curr;
+const operate = (chosenOperator, prev, curr) => {
+    let result = 0;
+    if (chosenOperator == "add") {
+        result = add(prev, curr);
+    } else if (chosenOperator == "subtract") {
+        result = subtract(prev, curr);
+    } else if (chosenOperator == "multiply") {
+        result = multiply(prev, curr);
+    } else if(chosenOperator === "divide" && curr === 0) {
+        alert("You are unable to divide by zero under the contrains of this calculator. Please select a differnt input");
+        curr = 0;
+    } else if (chosenOperator == "divide") {
+       result = divide(prev, curr);
+    }
+    return result;
+}
 
-rockBtn.addEventListener("click", playGame);
-paperBtn.addEventListener("click", playGame);
-scissorsBtn.addEventListener("click", playGame);
-playAgainBtn.addEventListener("click", resetGame);
+//OPERATOR BUTTON FUNCTIONS
+const operatorFunctions = document.querySelectorAll(".function");
+operatorFunctions.forEach(operator => {
+    operator.addEventListener("click", updateOperator);
+});
 
-// Called by each the three buttons. Executes a single round and updates all of the neccesary DOM
-function playGame(e) {
-    if (playerWins === 5 || computerWins === 5) {
-        alert("The maximum score count has been reached. The game will be automatically reset.");
-        resetGame();
-        return;
+function updateOperator (e) {
+    if (prev !== 0 && chosenOperator !== "" && curr !== 0) {
+        equalsOperation();
+        chosenOperator = e.target.classList[0];
+        display.innerText = "0";
     } else {
-        playerSelection(e);
-        resultMessage.innerText = playRound(playerInput, computerPlay())
-        winCounter.innerText = `${playerWins}`;
-        loseCounter.innerText = `${computerWins}`;
-        drawCounter.innerText = `${draws}`;
-        bigWinner();
+        chosenOperator = e.target.classList[0];
+        display.innerText = "0";
     }
+    console.log(display.innerText, prev, chosenOperator, curr);
+    return chosenOperator;
 }
 
-// Resets the game when the button is pressed
-function resetGame (e) {
-    let playerChoiceResult = document.querySelector(".playerResult");
-    let computerchoiceResult = document.querySelector(".computerResult");
-    let winnerMessage = document.querySelector(".bigWin");
+//OPPERATOR AND EQUALS KEYBOARD FUNCTION
+document.addEventListener("keydown", operatorKeyboad);
 
-    playerInput = "";
-    playerWins = 0;
-    computerWins = 0;
-    draws = 0;
-
-    playerChoiceResult.innerText = "";
-    computerchoiceResult.innerText = "";
-    resultMessage.innerText = "";
-    winCounter.innerText = "";
-    loseCounter.innerText = "";
-    drawCounter.innerText = "";
-    winnerMessage.innerText = "";
-}
-
-
-// To determine which button (Rock, Paper, Scissors) the player selected
-function playerSelection (event) {
-    let playerChoiceResult = document.querySelector(".playerResult");
-
-    if (event.target.classList.contains("rock")) {
-        playerInput = "Rock";
-    } else if (event.target.classList.contains("paper")) {
-        playerInput = "Paper";
-    } else if (event.target.classList.contains("scissors")) {
-        playerInput = "Scissors";
+function operatorKeyboad(e) {
+    let keyboardCode = e.keyCode;
+    if (keyboardCode === 107 || (keyboardCode === 187 && e.shiftKey === true)) {
+        chosenOperator = "add";
+        display.innerText = "0";
+    } else if (keyboardCode === 109 || keyboardCode === 189) {
+        chosenOperator = "subtract";
+        display.innerText = "0";
+    } else if (keyboardCode === 106 || (keyboardCode === 56 && e.shiftKey === true)) {
+        chosenOperator = "multiply";
+        display.innerText = "0";
+    } else if (keyboardCode === 111 || keyboardCode === 191) {
+        chosenOperator = "divide";
+        display.innerText = "0";
+    } else {
+        chosenOperator = chosenOperator;
+        display.innerText = display.innerText;
     }
-    return playerChoiceResult.innerText = ` ${playerInput}`;
+    return chosenOperator;
 }
 
-// This is the computer player who choses randomly from an array
-function computerPlay () {
-    let choices = ["Rock", "Paper", "Scissors"];
-    let computerchoiceResult = document.querySelector(".computerResult");
+//NUMBER BUTTON EVENT LISTENERS
+const numberBtn = document.querySelectorAll(".number");
+numberBtn.forEach(numberBtn => {
+    numberBtn.addEventListener("click", updateDisplay);
+});
+
+//DISPLAY AND NUMBER VALUE LOGIC
+function updateDisplay(e) {
+
+    //STOPS THE USER FROM ADDING NUMBERS PAST THE DISPLAY BOX
+    if (display.innerText.length >11) {
+        numberBtn.forEach(numberBtn => {
+            numberBtn.removeEventListener("click", updateDisplay);
+        })
+    }
+
+    if (display.innerText === 0) {
+        display.innerText = "0";
+    } else if (prev === 0 || chosenOperator === "") {
+        display.innerText = display.innerText.replace(/^0+/, '');
+        display.innerText += e.target.innerText;
+        prev = parseFloat(display.innerText);
+    } else {
+        display.innerText = display.innerText.replace(/^0+/, '');
+        display.innerText += e.target.innerText;
+        curr = parseFloat(display.innerText);
+    }
     
-    let randomSelect = Math.floor(Math.random() * choices.length);
-    computerchoiceResult.innerText = ` ${choices[randomSelect]}`;
-    return choices[randomSelect]; 
+    console.log(display.innerText.length, prev, chosenOperator, curr);
 }
 
-// This function plays a single round of Rock, Paper, Scissors
-function playRound (player, computer) {
-    // Use toUppereCase() so inputs are equal
-    player = player.toUpperCase();
-    computer = computer.toUpperCase();
+//ADDING NUMBER AND DECIMAL KEYBOARD FUNCTIONALITY
+document.addEventListener("keydown", keyboardFunction);
 
-    if (player === computer){
-        draws += 1;
-        return `It's a draw. You picked ${player} and the computer picked ${computer}.`;
-    } else if (player === "ROCK" && computer === "PAPER") {
-        computerWins += 1;
-        return `Sorry, you lose. You picked ${player} and the computer picked ${computer}.`;
-    } else if (player === "PAPER" && computer === "SCISSORS") {
-        computerWins += 1;
-        return `Sorry, you lose. You picked ${player} and the computer picked ${computer}.`;
-    } else if (player === "SCISSORS" && computer === "ROCK") {
-        computerWins += 1;
-        return `Sorry, you lose. You picked ${player} and the computer picked ${computer}.`;
-    } else if (player === "PAPER" && computer === "ROCK") {
-        playerWins += 1;
-        return `Yay, you win. You picked ${player} and the computer picked ${computer}.`;
-    } else if (player === "SCISSORS" && computer === "PAPER") {
-        playerWins += 1;
-        return `Yay, you win. You picked ${player} and the computer picked ${computer}.`;
-    } else if (player === "ROCK" && computer === "SCISSORS") {
-        playerWins += 1;
-        return `Yay, you win. You picked ${player} and the computer picked ${computer}.`;
+function keyboardFunction(e) {
+    let keyPress = String.fromCharCode(e.keyCode);
+
+    //STOPS THE USER FROM ADDING NUMBERS PAST THE DISPLAY BOX
+    if (display.innerText.length >12) {
+        document.removeEventListener("keydown", keyboardFunction);
+    }
+
+    if (keyPress == 0) {
+        if (display.innerText === 0) {
+            display.innerText = "0";
+        } else if (prev === 0 || chosenOperator === "") {
+            display.innerText = display.innerText.replace(/^0+/, '');
+            display.innerText = "0";
+            prev = parseFloat(display.innerText);
+        } else {
+            display.innerText = display.innerText.replace(/^0+/, '');
+            display.innerText += "0";
+            curr = parseFloat(display.innerText);
+        }
+    } else if (keyPress == 1) {
+        if (prev === 0 || chosenOperator === "") {
+            display.innerText = display.innerText.replace(/^0+/, '');
+            display.innerText += "1";
+            prev = parseFloat(display.innerText);
+        } else {
+            display.innerText = display.innerText.replace(/^0+/, '');
+            display.innerText += "1";
+            curr = parseFloat(display.innerText);
+        }
+    } else if (keyPress == 2) {
+        if (prev === 0 || chosenOperator === "") {
+            display.innerText = display.innerText.replace(/^0+/, '');
+            display.innerText += "2";
+            prev = parseFloat(display.innerText);
+        } else {
+            display.innerText = display.innerText.replace(/^0+/, '');
+            display.innerText += "2";
+            curr = parseFloat(display.innerText);
+        }
+    } else if (keyPress == 3) {
+        if (prev === 0 || chosenOperator === "") {
+            display.innerText = display.innerText.replace(/^0+/, '');
+            display.innerText += "3";
+            prev = parseFloat(display.innerText);
+        } else {
+            display.innerText = display.innerText.replace(/^0+/, '');
+            display.innerText += "3";
+            curr = parseFloat(display.innerText);
+        }
+    } else if (keyPress == 4) {
+        if (prev === 0 || chosenOperator === "") {
+            display.innerText = display.innerText.replace(/^0+/, '');
+            display.innerText += "4";
+            prev = parseFloat(display.innerText);
+        } else {
+            display.innerText = display.innerText.replace(/^0+/, '');
+            display.innerText += "4";
+            curr = parseFloat(display.innerText);
+        }
+    } else if (keyPress == 5) {
+        if (prev === 0 || chosenOperator === "") {
+            display.innerText = display.innerText.replace(/^0+/, '');
+            display.innerText += "5";
+            prev = parseFloat(display.innerText);
+        } else {
+            display.innerText = display.innerText.replace(/^0+/, '');
+            display.innerText += "5";
+            curr = parseFloat(display.innerText);
+        }
+    } else if (keyPress == 6) {
+        if (prev === 0 || chosenOperator === "") {
+            display.innerText = display.innerText.replace(/^0+/, '');
+            display.innerText += "6";
+            prev = parseFloat(display.innerText);
+        } else {
+            display.innerText = display.innerText.replace(/^0+/, '');
+            display.innerText += "6";
+            curr = parseFloat(display.innerText);
+        }
+    } else if (keyPress == 7) {
+        if (prev === 0 || chosenOperator === "") {
+            display.innerText = display.innerText.replace(/^0+/, '');
+            display.innerText += "7";
+            prev = parseFloat(display.innerText);
+        } else {
+            display.innerText = display.innerText.replace(/^0+/, '');
+            display.innerText += "7";
+            curr = parseFloat(display.innerText);
+        }
+    } else if (keyPress == 8 && e.shiftKey === false) {
+        if (prev === 0 || chosenOperator === "") {
+            display.innerText = display.innerText.replace(/^0+/, '');
+            display.innerText += "8";
+            prev = parseFloat(display.innerText);
+        } else {
+            display.innerText = display.innerText.replace(/^0+/, '');
+            display.innerText += "8";
+            curr = parseFloat(display.innerText);
+        }
+    } else if (keyPress == 9) {
+        if (prev === 0 || chosenOperator === "") {
+            display.innerText = display.innerText.replace(/^0+/, '');
+            display.innerText += "9";
+            prev = parseFloat(display.innerText);
+        } else {
+            display.innerText = display.innerText.replace(/^0+/, '');
+            display.innerText += "9";
+            curr = parseFloat(display.innerText);
+        }
+    } else {
+        display.innerText = display.innerText;
+    }
+    return display;
+}
+
+//DECIMAL BUTTON FUNCTION AND LOGIC
+const decimalBtn = document.querySelector(".decimal");
+decimalBtn.addEventListener("click", decimalNumber);
+
+function decimalNumber(e) {
+    while(!display.innerText.includes(".")) {
+        if(display.innerText === "") {
+            display.innerText += 0;
+        }
+        display.innerText += e.target.innerText;
+        if (prev === 0 || chosenOperator === "") {
+            prev = parseFloat(display);
+        } else {
+            curr = parseFloat(display);
+        }
     }
 }
 
-// Announces the first person to reach a score of 5
-function bigWinner() {
-    let winnerMessage = document.querySelector(".bigWin");
+//DECIMAL BUTTON KEYBOARD SUPPORT
+document.addEventListener("keydown", decimalKeyboard);
 
-    if (playerWins === 5) {
-        winnerMessage.innerText = "Congratulations! You have beaten the computer! If you would like to play again, press the button bellow.";
-    } else if (computerWins === 5) {
-        winnerMessage.innerText = "Better luck next time! The computer beat you! If you would like to play again, press the button bellow.";
+function decimalKeyboard (e) {
+    let keyboardCode = e.keyCode
+    if (keyboardCode === 110 || (keyboardCode === 190 && e.shiftKey === false)) {
+        while(!display.innerText.includes(".")) {
+            if(display.innerText === "") {
+                display.innerText += 0;
+            }
+            display.innerText += ".";
+            if (prev === 0 || chosenOperator === "") {
+                prev = parseFloat(display);
+            } else {
+                curr = parseFloat(display);
+            }
+        }
     } else {
-        winnerMessage.innerText = "";
+        display.innerText = display.innerText;
+        return;
+    }
+}
+
+//EQUALS BUTTON FUNCTION AND LOGIC
+const equals = document.querySelector(".equals");
+equals.addEventListener("click", equalsOperation);
+
+function equalsOperation() {
+    let equalsValue = 0;
+    if (prev !== 0 && chosenOperator !== "" && curr === 0) {
+        if (chosenOperator === "divide") {
+            curr = 0;
+        } else {
+            curr = prev;
+        }
+        equalsValue = operate(chosenOperator, prev, curr);
+        if (equalsValue > 99999999999 || equalsValue < -9999999999) {
+            equalsValue = equalsValue.toExponential(2);
+        } else {
+            equalsValue = parseFloat((equalsValue).toPrecision(12));
+        }
+    } else {
+        equalsValue = operate(chosenOperator, prev, curr);
+        if (equalsValue > 99999999999 || equalsValue < -9999999999) {
+            equalsValue = equalsValue.toExponential(2);
+        } else {
+            equalsValue = parseFloat((equalsValue).toPrecision(12));
+        }
+    }
+
+    display.innerText = `${equalsValue}`;
+
+    if (Number.isInteger(parseInt(curr))) {
+        prev = equalsValue;
+        curr = 0;
+        chosenOperator = "";
+    }
+    console.log(display.innerText, prev, chosenOperator, curr);
+    return equalsValue;
+}
+
+//EQUALS BUTTON KEYBOAD SUPPORT
+document.addEventListener("keydown", equalsKeyboard);
+
+function equalsKeyboard(e){
+    let keyboardCode = e.keyCode
+    if (keyboardCode === 13 || keyboardCode === 187) {
+        equalsOperation();
+    } else {
+        return
+    }
+}
+
+//CLEAR ALL BUTTON FUNCTION AND LOGIC
+const clear = document.querySelector(".clear");
+clear.addEventListener("click", clearHistory);
+
+function clearHistory () {
+    display.innerText = "0";
+    prev = 0;
+    curr = 0;
+    chosenOperator = "";
+    console.log(display.innerText, prev, chosenOperator, curr);
+}
+
+//CLEAR ALL BUTTON KEYBOARD SUPPORT
+document.addEventListener("keydown", clearKeyboard);
+
+function clearKeyboard(e){
+    let keyboardCode = e.keyCode
+
+    if (keyboardCode === 67) {
+        clearHistory ();
+    } else {
+        return
+    }
+}
+
+//CORRECTION KEY
+const correct = document.querySelector(".correct");
+correct.addEventListener("click", backSpace);
+
+function backSpace() {
+    display.innerText = display.innerText.substring(0, display.innerText.length - 1);
+    if (display.innerText.length === 0) {
+        display.innerText = "0";
+    }
+}
+
+//CORRECTION KEYBOARD SUPPORT
+document.addEventListener("keydown", backSpaceKeyboard);
+
+function backSpaceKeyboard(e) {
+    let keyboardCode = e.keyCode;
+
+    if (keyboardCode === 8 || keyboardCode === 46) {
+        backSpace();
+    } else {
+        return;
     }
 }
